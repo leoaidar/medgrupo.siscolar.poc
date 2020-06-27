cbfe27f6863125e603ae68761503e7bd2e7c7525
Léo Aidar <leofla@gmail.com>
2020-06-27 20:12:19 -0300

    School routing list and form

--- Presentation/Siscolar/src/app/pages/schools/schools-routing.module.ts
+++ Presentation/Siscolar/src/app/pages/schools/schools-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SchoolListComponent } from './school-list/school-list.component';
+import { SchoolFormComponent } from './school-form/school-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SchoolListComponent },
+  { path: 'new', component: SchoolFormComponent },
+  { path: ':id/edit', component: SchoolFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
