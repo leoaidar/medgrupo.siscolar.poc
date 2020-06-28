2c92efee0303db185bdd3341a11a42c3bda2d209
Léo Aidar <leofla@gmail.com>
2020-06-27 23:25:22 -0300

    Adding School Model

--- Presentation/Siscolar/src/app/in-memory-database.ts
+++ Presentation/Siscolar/src/app/in-memory-database.ts
@@ -1,9 +1,10 @@
-import { InMemoryDbService } from "angular-in-memory-web-api";
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { School } from './pages/schools/shared/school-model';
 
 export class InMemoryDatabase implements InMemoryDbService {
 
  createDb(){
-  const schools = [
+  const schools = School[] = [
    { id: '6D1E0197-90AF-4985-A5FE-828F28CFB06D', name: 'Escola João', schoolPrincipal: 'Sr. Aidar', maxSchoolClass: 10 },
    { id: '6D1E0197-90AF-4985-A5FE-828F28CFB06D', name: 'Escola João', schoolPrincipal: 'Sr. Aidar', maxSchoolClass: 10 },
    { id: '6D1E0197-90AF-4985-A5FE-828F28CFB06D', name: 'Escola João', schoolPrincipal: 'Sr. Aidar', maxSchoolClass: 10 }
---
+++ Presentation/Siscolar/src/app/pages/schools/shared/school-model.ts
@@ -0,0 +1,8 @@
+export class School{
+    constructor(
+        public id?: string,
+        public name?: string,
+        public schoolPrincipal?: string,
+        public maxSchoolClass?: number
+    ){}
+}
