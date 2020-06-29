6e90b11ea3555a4791f5d651afa2b2eedacb1edb
Léo Aidar <leofla@gmail.com>
2020-06-29 12:46:30 -0300

    setting 5011 port to localhost

--- Medgrupo.Siscolar.Api/Program.cs
+++ Medgrupo.Siscolar.Api/Program.cs
@@ -14,7 +14,7 @@ namespace Medgrupo.Siscolar.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://*:5011");
                 });
     }
 }
